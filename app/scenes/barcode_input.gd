cd9daa509f79c7a18da5d3c1620ccc55208e3043
641i130 <galileoray@gmail.com>
2024-03-05 23:03:36 -0600

    can read and print barcodes

--- app/scenes/barcode_input.gd
+++ app/scenes/barcode_input.gd
@@ -2,10 +2,17 @@ extends TextEdit
 
 
 # Called when the node enters the scene tree for the first time.
+
 func _ready():
+	# Connect the text_changed signal to the _on_text_changed function
 	self.grab_focus()
 
+func _on_text_changed():
+	# Check if the last character is a newline character
+	if text.ends_with("\n"):
+		# Clear the TextEdit
+		print(self.text)
+		clear()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
