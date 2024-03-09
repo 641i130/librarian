030391f0ee2a4a31ee3251f505eecc07a566ce95
641i130 <galileoray@gmail.com>
2024-03-09 16:38:41 -0600

    books titles

--- app/scenes/barcode_input.gd
+++ app/scenes/barcode_input.gd
@@ -3,6 +3,9 @@ extends TextEdit
 func _ready():
 	# Connect the text_changed signal to the _on_text_changed function
 	self.grab_focus()
+	for book in data_load():
+		# Should read in other things besides code
+		spawn_book(book[0]) # Use string instead of array
 
 func save(content):
 	var file = FileAccess.open("user://data.csv", FileAccess.WRITE | FileAccess.READ)
@@ -15,23 +18,38 @@ func save(content):
 		file.store_string(content)
 		file.close()
 
-func load():
-	var file = FileAccess.open("user://data.csv", FileAccess.READ)
-	var content = file.get_as_text()
-	return content
+func data_load():
+	var file = FileAccess.open("user://data.csv", FileAccess.WRITE | FileAccess.READ)
+	if file != null:
+		var books = []
+		while !file.eof_reached():
+			var line = file.get_csv_line()
+			print(line)
+			books.append(line)
+		return books
+	else:
+		return []
+	
+
+func spawn_book(code):
+	"Spawns a book record"
+	code = code.replace("\n","")
+	if code != "":
+		var book = load("res://scenes/book_record.tscn").instantiate()
+		book.get_node("Box/Info/Barcode").text = code
+		# Using the code we should retrieve other info about book and import
+		# We can add toggles for specific book engines later
+		book.get_node("Box/Info/Title").text = "Title: "
+		book.get_node("Box/Info/Author").text = "Author: "
+		book.get_node("Box/Info/Count").text = "Count: 1"
+		get_node("/root/Main/CanvasLayer/ScrollBox/List").add_child(book)
 
 func _on_text_changed():
 	# Check if the last character is a newline character
 	if text.ends_with("\n"):
-		print(self.text)
-		save(self.text)
 		# Make queue of book record
-		var book = load("res://scenes/book_record.tscn").instantiate()
-		book.get_node("Button").text = self.text
-		get_node("/root/Main/CanvasLayer/ScrollBox/List").add_child(book)
-		# Add to database
-		# Once its in the database, search ISBN or kinokuniya for the record data and display? or put into database
-		
+		spawn_book(self.text)
+		save(self.text)
 		# Clear the TextEdit
 		clear()
 
